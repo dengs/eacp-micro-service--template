4535e3e1b1ef100a5c11fef33a8d2475d2f1fad0
fsk <fsk@hanzhisoft.com>
2023-08-16 16:11:06 +0800

    3.0.0

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-## [eacp-micro-service--template](http://microdev.realty.hanzhigroup.cn:9024/EACP/eacp-micro-service--template) 
+## eacp-micro-service--template
 
 EACP平台 - 微服务应用模板
--- src/main/java/com/hanzhisoft/config/ClientWebsecurityConfigurer.java
+++ src/main/java/com/hanzhisoft/config/ClientWebsecurityConfigurer.java
@@ -1,5 +1,6 @@
 package com.hanzhisoft.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.ObjectPostProcessor;
@@ -12,6 +13,12 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ClientWebsecurityConfigurer extends WebSecurityConfigurerAdapter {
 
+    @Value("${client.http.security.authorize.matchers:/,/*/**}")
+    private String[] authorizeMatchers = {};
+
+    @Value("${client.web.security.ignoring.matchers:/css/**,/js/**,/favicon.ico,/static/**,/error}")
+    private String[] ignoringMatchers = {};
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
@@ -19,36 +26,28 @@ public class ClientWebsecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     /**
      * 访问静态资源
+     *
+     * @param web
      */
     @Override
     public void configure(WebSecurity web) {
-        web.ignoring().antMatchers(
-                "/css/**",
-                "/js/**",
-                "/favicon.ico",
-                "/static/**",
-                "/error");
+        web.ignoring().antMatchers(ignoringMatchers);
     }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/", "/*/**").permitAll()
+        http.authorizeRequests()
+                .antMatchers(authorizeMatchers).permitAll()
                 .anyRequest().authenticated()
-                .withObjectPostProcessor(urlObjectPostProcessor());
-
-
+                .withObjectPostProcessor(
+                        new ObjectPostProcessor<FilterSecurityInterceptor>() {
+                            @Override
+                            public <O extends FilterSecurityInterceptor> O postProcess(O o) {
+                                return o;
+                            }
+                        }
+                );
         // 不加会导致退出 不支持GET方式
         http.csrf().disable();
-    }
-
-    public ObjectPostProcessor urlObjectPostProcessor() {
-        return new ObjectPostProcessor<FilterSecurityInterceptor>() {
-            @Override
-            public <O extends FilterSecurityInterceptor> O postProcess(O o) {
-                return o;
-            }
-        };
     }
 }
